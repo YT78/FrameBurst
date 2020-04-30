0d372e44ea4e4a581f378ac3e8a1b69dc81392b7
Epichunter78 <52764778+Epichunter78@users.noreply.github.com>
2020-04-29 23:51:13 -0400

    All Accent Core + R Characters Frame Data Page Implementation
    Frame Data pages have been implemented for all Accent Core +R Characters

--- app/src/main/java/com/example/frameburst/acr/acrJusticeActivity.java
+++ app/src/main/java/com/example/frameburst/acr/acrJusticeActivity.java
@@ -1,31 +1,67 @@
-package com.example.frameburst;
+package com.example.frameburst.acr;
 
 import android.os.Bundle;
+import android.view.View;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.Button;
+
+import com.example.frameburst.R;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 public class acrJusticeActivity extends AppCompatActivity {
-    private WebView webview;
+    private WebView webview, webview2;
+    private ConstraintLayout profile, framedata;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_acr_justice);
+
+
         webview = (WebView) findViewById(R.id.webview);
         webview.setWebViewClient(new WebViewClient());
         webview.loadUrl("https://guilty-gear.fandom.com/wiki/Justice");
+
+        webview2 = (WebView) findViewById(R.id.webview2);
+        webview2.setWebViewClient(new WebViewClient());
+        webview2.loadUrl("https://www.dustloop.com/wiki/index.php?title=GGACR/Justice");
+
+        final ConstraintLayout profile = findViewById(R.id.layout2);
+        final ConstraintLayout framedata = findViewById(R.id.layout3);
+
+        Button button1 = findViewById(R.id.button);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                profile.setVisibility(View.GONE);
+                framedata.setVisibility(View.VISIBLE);
+            }
+        });
+
+        Button button2 = findViewById(R.id.button2);
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                profile.setVisibility(View.VISIBLE);
+                framedata.setVisibility(View.GONE);
+            }
+        });
 
         WebSettings webSettings = webview.getSettings();
         webSettings.setJavaScriptEnabled(true);
     }
 
+
     @Override
     public void onBackPressed() {
-        if (webview.canGoBack()){
+        if (webview.canGoBack()) {
             webview.goBack();
-        }else{
+        } else {
             super.onBackPressed();
         }
 
--- app/src/main/java/com/example/frameburst/acr/acrKyActivity.java
+++ app/src/main/java/com/example/frameburst/acr/acrKyActivity.java
@@ -1,31 +1,66 @@
-package com.example.frameburst;
+package com.example.frameburst.acr;
 
 import android.os.Bundle;
+import android.view.View;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.Button;
+
+import com.example.frameburst.R;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 public class acrKyActivity extends AppCompatActivity {
-    private WebView webview;
+    private WebView webview, webview2;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_acr_ky);
+
+
         webview = (WebView) findViewById(R.id.webview);
         webview.setWebViewClient(new WebViewClient());
         webview.loadUrl("https://guilty-gear.fandom.com/wiki/Ky_Kiske");
+
+        webview2 = (WebView) findViewById(R.id.webview2);
+        webview2.setWebViewClient(new WebViewClient());
+        webview2.loadUrl("https://www.dustloop.com/wiki/index.php?title=GGACR/Ky_Kiske");
+
+        final ConstraintLayout profile = findViewById(R.id.layout2);
+        final ConstraintLayout framedata = findViewById(R.id.layout3);
+
+        Button button1 = findViewById(R.id.button);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                profile.setVisibility(View.GONE);
+                framedata.setVisibility(View.VISIBLE);
+            }
+        });
+
+        Button button2 = findViewById(R.id.button2);
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                profile.setVisibility(View.VISIBLE);
+                framedata.setVisibility(View.GONE);
+            }
+        });
 
         WebSettings webSettings = webview.getSettings();
         webSettings.setJavaScriptEnabled(true);
     }
 
+
     @Override
     public void onBackPressed() {
-        if (webview.canGoBack()){
+        if (webview.canGoBack()) {
             webview.goBack();
-        }else{
+        } else {
             super.onBackPressed();
         }
 
